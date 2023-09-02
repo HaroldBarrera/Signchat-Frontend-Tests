c9cb602601a703cdb2ad2a32df7b668d20e0527c
HaroldBarrera <hsbarreran@academia.usbbog.edu.co>
2023-09-02 16:42:31 -0500

    Traduccion de texto a imagenes

--- react-signchat-test/src/App.js
+++ react-signchat-test/src/App.js
@@ -9,15 +9,15 @@ function App() {
   const [inputImgValue, setInputImgValue] = useState([]);
 
   const handleButtonClick = async (key) => {
-    //let letter = await key.target.alt;
-    //setInputValue(inputValue + letter);
     let imageUrl = key.target.src;
-    setInputImgValue([...inputImgValue, imageUrl]);
+    if(imageUrl != null){
+      setInputImgValue([...inputImgValue, imageUrl]);
+    }
   };
 
   const sendImagesToBackend = async () => {
     try {
-      const response = await fetch('http://localhost:8080/api/messages/translate', {
+      const response = await fetch('http://localhost:8080/api/messages/translate/img-txt', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -38,6 +38,29 @@ function App() {
     }
   }
 
+  const sendTextToBackend = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/messages/translate/txt-img', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: inputValue }),
+      });
+  
+      if (response.ok) {
+        console.log(response);
+        const translatedData = await response.json();
+        setInputImgValue(translatedData);
+        console.log('Mensaje enviado con éxito al backend.');
+      } else {
+        console.error('Error al enviar el mensaje al backend.');
+      }
+    } catch (error) {
+      console.error('Error al enviar el mensaje al backend:', error);
+    }
+  }
+
   return (
     <>
       <h1>Prueba de Signchat Microservicio</h1>
@@ -46,9 +69,10 @@ function App() {
         <img key={index} src={url} alt={`Imagen ${index}`} />
       ))}
       <br />
-      <button onClick={sendImagesToBackend}>Traducir</button>
+      <button onClick={sendImagesToBackend}>Traducir imagenes</button>
+      <button onClick={sendTextToBackend}>Traducir texto</button>
       <br />
-      <input type='text' value={inputValue} readOnly />
+      <input type='text' value={inputValue} onChange={(e) => {setInputValue(e.target.value)}}/>
       <KeyboardComponent handleKeyClick={handleButtonClick} />
     </>
   );
