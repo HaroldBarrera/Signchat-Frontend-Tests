951e6ba7f50c93289514436780d0f3fd114baef5
WilliamDavid2 <wdflorezl@academia.usbbog.edu.co>
2023-09-10 13:10:13 -0500

    Prueba PHP
    Si corro el archivo .php es xampp lo ejecuta bien(se ve el teclado en la paguina), pero fuera de la carpeta de xampp no(parece ser algo sobre que tomcat no puede ejecutar php ).
     El documento .html no puede ejecutar php ademas que tomcat tampoco esta echo para php asi que trataré de modificar el pom del api para que pueda usar php(anotaré los cambios en un documento word que le mandraé por correo)

--- php-signchat-test/Prueba_Index.php
+++ php-signchat-test/Prueba_Index.php
@@ -1,27 +1,47 @@
-<?php
-// $servername = "192.168.1.103";
-$servername = "172.17.3.181"; // en la Univercidad
-$username = "ADMIN";
-$password = "";
-$database = "signchatprueba";
-
-// Crear una conexión
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chat</title>
+    <style>
+        /* Estilos CSS para el chat */
+        /* Estilo básico del contenedor del chat */
+        .Div-Chat {
+            width: 300px;
+            margin: 0 auto;
+            height: 600px;
+            border: 1px solid #ccc;
+            padding: 10px;
+            overflow-y: scroll; /* Agregamos una barra de desplazamiento vertical */
+            max-height: 400px; /* Altura máxima del contenedor del chat */
+        }
+
+        /* Estilo del campo de entrada */
+        .input-field {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-top: none; /* Quitamos el borde superior para que se integre con el contenedor de chat */
+            border-radius: 0 0 5px 5px; /* Estilo de borde en la parte inferior */
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="Div-Chat" id="chat-container">
+        <!-- Contenedor de mensajes del chat -->
+    </div>
+    <!-- Campo de entrada para escribir mensajes -->
+    <input type="text" class="input-field" id="message-input" placeholder="Escribe tu mensaje aquí">
+    <br>
+    <div class="input-field" id="keyboard-container">
+        <?php
+        // Obtener el contenido de "http://localhost:8080/keyboard" utilizando PHP
+        $contenido = file_get_contents('http://localhost:8080/keyboard');
+
+        // Imprimir el contenido dentro de la página
+        echo $contenido;
+        ?>
+    </div>
 </body>
 </html>
